1aeb3bb388b2e59f87da25ab215afa6e3516333b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-14 18:34:37 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route exact path="/game" component={ Game } />
         </Switch>
       </div>
     );
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
-export const login = () => ({
-  type: 'LOGIN',
+import * as types from './actionTypes';
+
+export const tokenRequestSuccess = ({ token }) => ({
+  type: types.TOKEN_REQUEST_SUCCESS,
+  token,
 });
 
-export const xablau = () => ({
-  type: '',
+export const tokenRequestError = (errorData) => ({
+  type: types.TOKEN_REQUEST_ERROR,
+  errorData,
 });
+
+// export const login = (loginData) => ({
+//   type: types.LOGIN,
+//   loginData,
+// });
+
+export function requestToken() {
+  return (dispatch) => fetch('https://opentdb.com/api_token.php?command=request')
+    .then((response) => response.json())
+    .then((token) => dispatch(tokenRequestSuccess(token)))
+    .catch((error) => dispatch(tokenRequestError(error)));
+}

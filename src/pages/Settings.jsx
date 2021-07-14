4bbf0a65692c54e2cffd1cd89320b3ecfd661f95
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-14 19:05:04 -0300

    Requisito 3

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações do jogo</h1>
+      </div>
+    );
+  }
+}

274a6a1a5f6f30a86d75dcef52cd8a6c3822ddeb
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-19 17:09:25 -0300

    Requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
+    const { sectionPlayer } = this.props;
+    const { name, score, gravatarEmail } = sectionPlayer;
+    const gravatarHash = md5(gravatarEmail).toString();
+
     return (
-      <div>
-        <p data-testid="feedback-text">feedback</p>
+      <div data-testid="feedback-text">
+        <h1>
+          {'Jogador: '}
+          <span data-testid="header-player-name">{ name }</span>
+        </h1>
+        <img
+          alt="avatar"
+          src={ `https://www.gravatar.com/avatar/${gravatarHash}` }
+          data-testid="header-profile-picture"
+        />
+        <p>
+          Pontuação:
+          <span
+            data-testid="header-score"
+          >
+            {score}
+          </span>
+        </p>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  sectionPlayer: state.sectionReducer,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  sectionPlayer: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    gravatarEmail: PropTypes.string.isRequired,
+  }).isRequired,
+};

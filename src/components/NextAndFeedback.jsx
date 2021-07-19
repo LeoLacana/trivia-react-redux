9959f20c7d296d50024fa4248d9623b48560285c
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-19 16:28:22 -0300

    Requisito 11

---
+++ src/components/NextAndFeedback.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class NextAndFeedback extends Component {
+  render() {
+    const { index, length, nextQuestion, history } = this.props;
+    console.log(history);
+    return (
+      <button
+        onClick={ () => (index < length ? nextQuestion() : history.push('/feedback')) }
+        type="button"
+        data-testid="btn-next"
+      >
+        {index < length ? 'Próxima pergunta' : 'Feedback'}
+      </button>
+    );
+  }
+}
+
+NextAndFeedback.propTypes = {
+  index: PropTypes.number.isRequired,
+  length: PropTypes.number.isRequired,
+  nextQuestion: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="feedback-text">feedback</p>
+      </div>
+    );
+  }
+}
+
+export default Feedback;

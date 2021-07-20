e17b6f18a8bee321cb60671c18bcee94c67f3029
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-20 15:27:36 -0300

    Requisito 16

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path="/game" component={ Game } />
           <Route exact path="/settings" component={ Settings } />
           <Route exact path="/feedback" component={ Feedback } />
+          <Route exact path="/ranking" component={ Ranking } />
         </Switch>
       </div>
     );
---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
